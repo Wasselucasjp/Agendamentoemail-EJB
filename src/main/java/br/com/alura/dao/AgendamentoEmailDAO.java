fd158d14c1a82b0700511a1ea6a9b5b6e960e3ef
Wasse Lucas <wasselucas.silva@gmail.com>
2024-02-13 15:37:28 -0300

    criado metedo inserirna classe DAO

--- src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
+++ src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
@@ -2,13 +2,13 @@ package br.com.alura.dao;
 
 import java.util.List;
 
-import javax.ejb.Stateful;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.alura.entity.AgendamentoEmail;
 
-@Stateful
+@Stateless
 public class AgendamentoEmailDAO {
 	
 	@PersistenceContext
@@ -17,6 +17,10 @@ public class AgendamentoEmailDAO {
 	public List<AgendamentoEmail>listar(){
 		return entityManager.createQuery("SELECT ae FROM AgendamentoEmail ae",
 				AgendamentoEmail.class).getResultList();
+	}
+	
+	public void inserir(AgendamentoEmail  agendamentoEmail) {
+		entityManager.persist(agendamentoEmail);
 	}
 	
 }
